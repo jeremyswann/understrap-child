0810ed4e24afee14ec9871f1e2b97e37a2291a90
jeremyswann <jeremy@swanburger.com>
2019-05-31 15:46:40 -0700

    :zap: revert to default burger menu icon

--- header.php
+++ header.php
@@ -56,8 +56,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 					} ?><!-- end custom logo -->
 
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
-					<i class="fa fa-bars"></i>
-					<i class="fa fa-times"></i>
+					<span class="navbar-toggler-icon"></span>
 				</button>
 
 				<!-- The WordPress Menu goes here -->
