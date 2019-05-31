5b966410d3968439a51aa91263ec2bac113cce91
jeremyswann <jeremy@swanburger.com>
2019-05-31 14:26:08 -0700

    :zap: added theme styles

---
+++ header.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package understrap
+ */
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<div class="hfeed site" id="page">
+
+	<!-- ******************* The Navbar Area ******************* -->
+	<div class="wrapper-fluid wrapper-navbar bg-white" id="wrapper-navbar">
+
+		<a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e( 'Skip to content',
+		'understrap' ); ?></a>
+
+		<nav class="navbar navbar-expand-lg navbar-light">
+
+		<?php if ( 'container' == $container ) : ?>
+			<div class="container position-relative">
+		<?php endif; ?>
+					<!-- Your site title as branding in the menu -->
+					<?php if ( ! has_custom_logo() ) { ?>
+
+						<?php if ( is_front_page() && is_home() ) : ?>
+
+							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+							
+						<?php else : ?>
+
+							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+						
+						<?php endif; ?>
+						
+					
+					<?php } else {
+						the_custom_logo();
+					} ?><!-- end custom logo -->
+
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+					<i class="fa fa-bars"></i>
+					<i class="fa fa-times"></i>
+				</button>
+
+				<!-- The WordPress Menu goes here -->
+				<?php wp_nav_menu(
+					array(
+						'theme_location'  => 'primary',
+						'container_class' => 'collapse navbar-collapse',
+						'container_id'    => 'navbarNavDropdown',
+						'menu_class'      => 'navbar-nav',
+						'fallback_cb'     => '',
+						'menu_id'         => 'main-menu',
+						'walker'          => new understrap_WP_Bootstrap_Navwalker(),
+					)
+				); ?>
+			
+				<div class="position-absolute cta">
+					<a class="btn btn-primary mr-3" href="tel:1800100411"><i class="fa fa-phone text-secondary"></i> Talk To Us </a>
+					<a class="btn btn-primary" href="https://www.swanboroughfunerals.com.au/cremation-service/"><i class="fa fa-quote-left text-secondary" aria-hidden="true"></i> Instant Quote</a>
+				</div>
+			<?php if ( 'container' == $container ) : ?>
+			</div><!-- .container -->
+			<?php endif; ?>
+			
+			<div class="behind-nav"></div>
+
+		</nav><!-- .site-navigation -->
+
+	</div><!-- .wrapper-navbar end -->
